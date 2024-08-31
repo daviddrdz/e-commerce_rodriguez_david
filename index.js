14149b62bcdef2274fe5b620c28e07fda9a3ede0
David <monkey@donkey.com>
2024-08-31 01:28:59 -0600

    EDIT: CARDS CON JS

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hola mundo");
+const h3 = document.querySelectorAll("h3");
+const img = document.querySelectorAll("img");
+const precio = document.querySelectorAll(".precio");
+
+h3[0].innerText = "Radiohead";
+h3[1].innerText = "The Strokes"
+h3[2].innerText = "Arctic Monkeys"
+img[0].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ0r_jo4S9Z9GVhgQEzVlF3yqqRSCFz6nsDg&s";
+img[1].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq5SB-Sn2ZCl9HatONKUa8Fy8ursj4wDEcPA&s";
+img[2].src = "https://mapsound.ar/wp-content/uploads/2022/04/393bcbbd62f05dfc0c52e3d767514518.jpg";
+precio[0].innerText = "$1985";
+precio[1].innerText = "$1998";
+precio[2].innerText = "$2002";
