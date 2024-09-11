1a5760d3b4be572dc7f2e30e9876f5cfaf5fcd3e
David <monkey@donkey.com>
2024-09-10 18:07:01 -0600

    cards usando for

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-const h3 = document.querySelectorAll("h3");
-const img = document.querySelectorAll("img");
-const precio = document.querySelectorAll(".precio");
+const titulo = document.querySelector("h1").innerText = "Productos";
+let section = document.querySelector(".contenedor");
+let card = [];
 
-h3[0].innerText = "Radiohead";
-h3[1].innerText = "The Strokes"
-h3[2].innerText = "Arctic Monkeys"
-img[0].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQ0r_jo4S9Z9GVhgQEzVlF3yqqRSCFz6nsDg&s";
-img[1].src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq5SB-Sn2ZCl9HatONKUa8Fy8ursj4wDEcPA&s";
-img[2].src = "https://mapsound.ar/wp-content/uploads/2022/04/393bcbbd62f05dfc0c52e3d767514518.jpg";
-precio[0].innerText = "$1985";
-precio[1].innerText = "$1998";
-precio[2].innerText = "$2002";
+for (let i = 1; i < 10; i++) {
+    card.push(`
+            <div class="carta">
+                <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" height="200px">
+                <h3>Producto ${i}</h3>
+                    <div class="cajaprecio">
+                        <span class="precio">$$$</span>
+                    </div>
+                <div class="carrito">
+                    <button><i class='bx bx-cart'></i>Agregar al carrito</button>
+                </div>
+            </div>
+        `);
+}
+
+section.innerHTML = card.join().replaceAll(",", "");
