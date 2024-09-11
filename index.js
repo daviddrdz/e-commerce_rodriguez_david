989b0b014f993282ea4705de2352b68895c98234
David <monkey@donkey.com>
2024-09-10 18:12:21 -0600

    EDIT: CARDS USANDO FOR

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ let section = document.querySelector(".contenedor");
 let card = [];
 
 for (let i = 1; i < 10; i++) {
-    card.push(`
-            <div class="carta">
+    card.push(
+            `<div class="carta">
                 <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg" height="200px">
                 <h3>Producto ${i}</h3>
                     <div class="cajaprecio">
@@ -13,8 +13,8 @@ for (let i = 1; i < 10; i++) {
                 <div class="carrito">
                     <button><i class='bx bx-cart'></i>Agregar al carrito</button>
                 </div>
-            </div>
-        `);
+            </div>`
+        );
 }
 
 section.innerHTML = card.join().replaceAll(",", "");
