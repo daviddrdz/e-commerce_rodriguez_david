850728c5b70de4845a563e9c9a4089b5a3e800ce
david <davidrdzb05@gmail.com>
2024-10-10 17:52:53 -0600

    feat: implementar buscador con botones de buscar y limpiar

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ import data from './data.js';
 const titulo = (document.querySelector("h1").innerText = "Productos");
 let section = document.querySelector(".contenedor");
 
-function fillCards() {
-  let productos = data.map((product) => `
+let cards = data.map((product) => `
   <div class="carta">
       <img src="${product.img}" height="200px">
       <h3>${product.title}</h3>
@@ -16,7 +15,44 @@ function fillCards() {
       </div>
   </div>
   `)
-  section.innerHTML = productos.join("");
+section.innerHTML = cards.join("");
+
+const input = document.querySelector("input");
+const buttonFilter = document.querySelectorAll("button")[1];
+const buttonReset = document.querySelectorAll("button")[2];
+
+const filterList = () => {
+    let filtered = data.filter((item) => item.title === input.value);
+    cards = filtered.map((filter) => `
+        <div class="carta">
+            <img src="${filter.img}" height="200px">
+            <h3>${filter.title}</h3>
+                <div class="cajaprecio">
+                    <span class="precio">$${filter.price}</span>
+                </div>
+            <div class="carrito">
+                <button><a href=./producto.html?prod=${filter.id}><i class='bx bx-info-circle'></i>Ver más</a></button>
+            </div>
+        </div>
+    `)
+    section.innerHTML = cards.join("");
 }
 
-fillCards();
+const resetList = () => {
+    let cards = data.map((product) => `
+        <div class="carta">
+            <img src="${product.img}" height="200px">
+            <h3>${product.title}</h3>
+                <div class="cajaprecio">
+                    <span class="precio">$${product.price}</span>
+                </div>
+            <div class="carrito">
+                <button><a href=./producto.html?prod=${product.id}><i class='bx bx-info-circle'></i>Ver más</a></button>
+            </div>
+        </div>
+    `)
+    section.innerHTML = cards.join("");
+}
+
+buttonFilter.addEventListener("click", filterList);
+buttonReset.addEventListener("click", resetList);
