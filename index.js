1be3a5ded89b135630e53f1c2c1f432e94da3121
david <davidrdzb05@gmail.com>
2024-10-17 15:26:44 -0600

    feat: agregar funcion fillCards()

--- index.js
+++ index.js
@@ -2,81 +2,43 @@ import data from './data.js';
 
 const titulo = (document.querySelector("h1").innerText = "Productos");
 let section = document.querySelector(".contenedor");
-
-let cards = data.map((product) => `
-  <div class="carta">
-      <img src="${product.img}" height="200px">
-      <h3>${product.title}</h3>
-          <div class="cajaprecio">
-              <span class="precio">$${product.price}</span>
-          </div>
-      <div class="carrito">
-          <button><a href=./producto.html?prod=${product.id}><i class='bx bx-info-circle'></i>Ver más</a></button>
-      </div>
-  </div>
-  `)
-section.innerHTML = cards.join("");
-
 const input = document.querySelector("input");
 const buttonFilter = document.querySelector("#filter");
 const buttonReset = document.querySelector("#reset");
 const buttonAll = document.querySelector("#all");
 const categoryButtons = document.querySelectorAll(".categoryButton");
 
-const filterList = () => {
-    let filtered = data.filter((item) => item.title === input.value);
-    cards = filtered.map((filter) => `
+function fillCards(filter) {
+    let cards = filter.map((item) => `
         <div class="carta">
-            <img src="${filter.img}" height="200px">
-            <h3>${filter.title}</h3>
-                <div class="cajaprecio">
-                    <span class="precio">$${filter.price}</span>
-                </div>
+            <img src="${item.img}" height="200px">
+            <h3>${item.title}</h3>
+            <div class="cajaprecio">
+                <span class="precio">$${item.price}</span>
+            </div>
             <div class="carrito">
-                <button><a href=./producto.html?prod=${filter.id}><i class='bx bx-info-circle'></i>Ver más</a></button>
+                <button><a href=./producto.html?prod=${item.id}><i class='bx bx-info-circle'></i>Ver más</a></button>
             </div>
         </div>
     `)
     section.innerHTML = cards.join("");
 }
 
-const resetList = () => {
-    let cards = data.map((product) => `
-        <div class="carta">
-            <img src="${product.img}" height="200px">
-            <h3>${product.title}</h3>
-                <div class="cajaprecio">
-                    <span class="precio">$${product.price}</span>
-                </div>
-            <div class="carrito">
-                <button><a href=./producto.html?prod=${product.id}><i class='bx bx-info-circle'></i>Ver más</a></button>
-            </div>
-        </div>
-    `)
-    section.innerHTML = cards.join("");
+fillCards(data);
+
+const filterList = () => {
+    let filtered = data.filter((item) => item.title === input.value);
+    fillCards(filtered);
 }
 
 const filterCategory = (category) => {
     let itemCategory = data.filter((item) => item.category === category);
-    cards = itemCategory.map((filter) => `
-        <div class="carta">
-            <img src="${filter.img}" height="200px">
-            <h3>${filter.title}</h3>
-                <div class="cajaprecio">
-                    <span class="precio">$${filter.price}</span>
-                </div>
-            <div class="carrito">
-                <button><a href=./producto.html?prod=${filter.id}><i class='bx bx-info-circle'></i>Ver más</a></button>
-            </div>
-        </div>
-    `)
-    section.innerHTML = cards.join("");
+    fillCards(itemCategory);
 }
 
 buttonFilter.addEventListener("click", filterList);
-buttonReset.addEventListener("click", resetList);
-buttonAll.addEventListener("click", resetList);
-
+buttonReset.addEventListener("click", () => fillCards(data));
+buttonAll.addEventListener("click", () => fillCards(data));
 for (let i in categoryButtons) {
     categoryButtons[i].addEventListener("click", (event) => {
         const category = event.target.getAttribute("value");
