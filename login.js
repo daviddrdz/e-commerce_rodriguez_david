3d5256e16aede5944e530afd314fa9dcd08b4123
david <davidrdzb05@gmail.com>
2024-10-22 22:24:54 -0600

    feat: agregar login

---
+++ login.js
@@ -0,0 +1,16 @@
+let form = document.querySelector("form");
+let p = document.querySelector("p");
+let button = document.querySelector("button");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (LOGIN_USER.email === form.elements.email.value && LOGIN_USER.password === form.elements.password.value) {
+    localStorage.setItem("email", form.elements.email.value);
+    localStorage.setItem("password", form.elements.password.value);
+    location.href = "./index.html"
+  } else {
+    p.style.display = "flex";
+    form.elements.email.value = "";
+    form.elements.password.value = "";
+  }
+})
